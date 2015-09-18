dd7ab8fbd474e7cf3e9468eaafd0bc27a7e1cf60
bluepupu <bluepupu@users.noreply.github.com>
2015-09-18 09:58:51 +0800

    Add an date format shell script

---
+++ linuxBash/system/mydate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+if [ "$1" == "" ];then
+	echo "mydate.sh <million_seconds>"
+        echo "./mydate.sh 1420070436538"
+fi
+
+millionSeconds=${1:0:10}
+otherPart=${1:9:3}
+
+myFormat=`date +%Y/%m/%d\ %H:%M:%S -d\@$millionSeconds`".$otherPart"
+echo $myFormat
